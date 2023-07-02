ce3cf020ddc93879f5fa7f4048dae85b0b774fba
Rene <renatatwins18@gmail.com>
2023-07-02 14:30:38 +0300

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,21 +1,23 @@
-// const CORS_ALLOWED = [
-//   'http://localhost:3000',
-//   'https://api.movie-heyrene.nomoredomains.rocks',
-//   'http://api.movie-heyrene.nomoredomains.rocks',
-//   'https://heyrene.nomoredomains.monster',
-//   'https://api.heyrene.nomoredomains.monster',
-//   'http://api.heyrene.nomoredomains.monster',
-//   'http://heyrene.nomoredomains.monster',
-// ];
+const CORS_ALLOWED = [
+  'http://localhost:3000',
+  'https://api.movie-heyrene.nomoredomains.rocks',
+  'http://api.movie-heyrene.nomoredomains.rocks',
+  'https://heyrene.nomoredomains.monster',
+  'https://api.heyrene.nomoredomains.monster',
+  'http://api.heyrene.nomoredomains.monster',
+  'http://heyrene.nomoredomains.monster',
+];
 
 const handleCors = (req, res, next) => {
-  // const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
 
   const requestHeaders = req.headers['access-control-request-headers'];
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  // res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', '*');
+  if (CORS_ALLOWED.includes(origin)) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', '*');
+  }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
